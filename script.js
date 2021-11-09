ca32f0111720237081988e7bc2467da6c69424d2
Kauan Mesquita <73854606+KauanMesquita@users.noreply.github.com>
2021-11-08 23:29:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 function selectMode(mode) {
  
     var color = "ghostWhite";
-    var image = "/img/light-icon.png";
+    var image = "./img/light-icon.png";
     var label = "Light Mode";
     
     if (mode === "dark") {
      color = "darkSlateBlue";
-     image = "../img/dark-icon.png";
+     image = "./img/dark-icon.png";
      label = "Dark Mode";
      
     } else if (mode === "light") {
      color = "ghostWhite";
-     image = "../img/light-icon.png";
+     image = "./img/light-icon.png";
      label = "Light Mode";
      
     } else {
      color = "dimGray";
-     image = "../img/ninja-icon.png";
+     image = "./img/ninja-icon.png";
      label = "Ninja Mode";
     }
     
